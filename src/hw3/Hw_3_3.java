54b957a889cdc42d69cf2c9e2d5113d35c3126da
Mark Liu(TIA10211) <tzungneng@gmail.com>
2024-04-30 21:33:55 +0800

    update hw3 and hw4

--- src/hw3/Hw_3_3.java
+++ src/hw3/Hw_3_3.java
@@ -7,28 +7,21 @@ public class Hw_3_3 {
 
 	public static void main(String[] args) {
 		// 產生一個1~49的隨機亂數
-//		int random = (int) (Math.random() * 49) + 1;
 
 		// 讀取鍵盤輸入數值
 		Scanner s = new Scanner(System.in);
 		System.out.println("阿文...請輸入你討厭哪個數字");
 		int removeNum = s.nextInt();
-		
+
 //		int removeNum = 3;
 		int count = 0;
-
-//		int[] lottoNumber = new int[49];
 		int[] lottoNumber = new int[49];
 		for (int i = 0; i < lottoNumber.length; i++) {
-//			lottoNumber[i] = i + 1;
 			int lotto = i + 1;
-
 			if ((lotto % 10 != removeNum) && (lotto / 10) % 10 != removeNum) {
-//				System.out.print(lotto + " ");
 				lottoNumber[count] = lotto;
 				count++;
 			}
-//			System.out.println(lottoNumber[i]);
 		}
 		System.out.println();
 
@@ -36,27 +29,15 @@ public class Hw_3_3 {
 		Hw_3_3.showArray(lottoNumber);
 		System.out.println("總共有" + count + "個數字可選");
 
-//		// print array
-//		for (int i = 0; i < (lottoNumber.length); i++) {
-//			System.out.print(lottoNumber[i] + "\t");
-//			if ((i + 1) % 6 == 0)
-//				System.out.println();
-//		}
-
 		int[] out = new int[6];
-////		int[] out = {1,2,3,4,5,6};
+//		int[] out = {1,2,3,4,5,6};
 //
-//		for (int i = 0; i < out.length; i++) {
-//			int random = (int) (Math.random() * count) + 1;
-//			out[i] = lottoNumber[random];
-//		}
 		int k = 0;
 		boolean display = true;
 		boolean hasDuplicates = false;
 		while (display) {
 //			int random = (int) (Math.random() * count) + 1;
-			
-			
+
 			for (int i = 0; i < out.length; i++) {
 				int random = (int) (Math.random() * count) + 1;
 				out[i] = lottoNumber[random];
@@ -67,10 +48,11 @@ public class Hw_3_3 {
 				if (out[i] == out[i + 1]) {
 					hasDuplicates = true;
 					System.out.println("Number repeat");
-					break ;
+					break;
 				}
 			}
-			if(hasDuplicates == false) {
+			
+			if (hasDuplicates == false) {
 				display = false;
 			}
 
---
+++ src/hw4/Hw_4_3.java
@@ -0,0 +1,11 @@
+package hw4;
+
+public class Hw_4_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] str = {"mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"};
+		System.out.println(str[1]);
+	}
+
+}
